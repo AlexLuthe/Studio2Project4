fad027bfe62c521599b9b36df2a41555d4c56681
Levi Stenton <1005973@student.SAE.edu.au>
2017-04-12 21:06:19 +1000

    -Progress in outline shader

---
+++ Assets/Scripts/ShaderOutline.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderOutline : MonoBehaviour {
+
+    public EventListener eventListener;
+    public MissionObject[] missionObjects;
+
+	// Use this for initialization
+	void Start ()
+    {
+        missionObjects = FindObjectsOfType<MissionObject>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {		
+        foreach (MissionObject mobj in missionObjects)
+        {
+            for (int mObjIndex = 0; mObjIndex < eventListener.missionProg.Length; ++mObjIndex)
+            {
+                if (mobj.missionProg[1] == eventListener.missionProg[mObjIndex])
+                {
+                    mobj.gameObject.layer = 8;
+                    foreach (Transform child in mobj.gameObject.GetComponentInChildren<Transform>())
+                    {
+                        child.gameObject.layer = 8;
+                    }
+                }
+                else
+                {
+                    mobj.gameObject.layer = 0;
+                    foreach (Transform child in mobj.gameObject.GetComponentInChildren<Transform>())
+                    {
+                        child.gameObject.layer = 0;
+                    }
+                }
+            }
+        }
+	}
+}
