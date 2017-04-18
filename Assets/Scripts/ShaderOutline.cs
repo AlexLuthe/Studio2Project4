42e317de54d7c15cdebc8d9ab674c619454030fd
TyeBryant <tyebryant@hotmail.com>
2017-04-18 20:57:46 +1000

    Fixed highlight shader issues

--- Assets/Scripts/ShaderOutline.cs
+++ Assets/Scripts/ShaderOutline.cs
@@ -10,30 +10,42 @@ public class ShaderOutline : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
     {
-        missionObjects = FindObjectsOfType<MissionObject>();
+        
 	}
 	
 	// Update is called once per frame
 	void Update ()
-    {		
+    {
+        missionObjects = FindObjectsOfType<MissionObject>();
         foreach (MissionObject mobj in missionObjects)
         {
             for (int mObjIndex = 0; mObjIndex < eventListener.missionProg.Length; ++mObjIndex)
             {
-                if (mobj.missionProg[1] == eventListener.missionProg[mObjIndex])
+                if (mobj.missionProg[0] == mObjIndex)
                 {
-                    mobj.gameObject.layer = 8;
-                    foreach (Transform child in mobj.gameObject.GetComponentInChildren<Transform>())
+                    if (mobj.missionProg[1] == eventListener.missionProg[mObjIndex])
                     {
-                        child.gameObject.layer = 8;
+                        mobj.gameObject.layer = 8;
+                        foreach (Transform child in mobj.gameObject.GetComponentInChildren<Transform>())
+                        {
+                            child.gameObject.layer = 8;
+                            foreach (Transform kid in mobj.gameObject.GetComponentInChildren<Transform>())
+                            {
+                                kid.gameObject.layer = 8;
+                            }
+                        }
                     }
-                }
-                else
-                {
-                    mobj.gameObject.layer = 0;
-                    foreach (Transform child in mobj.gameObject.GetComponentInChildren<Transform>())
+                    else
                     {
-                        child.gameObject.layer = 0;
+                        mobj.gameObject.layer = 0;
+                        foreach (Transform child in mobj.gameObject.GetComponentInChildren<Transform>())
+                        {
+                            child.gameObject.layer = 0;
+                            foreach (Transform kid in mobj.gameObject.GetComponentInChildren<Transform>())
+                            {
+                                kid.gameObject.layer = 0;
+                            }
+                        }
                     }
                 }
             }
