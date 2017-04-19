81984e6aaaa3ace970b7933eb7c29cd41f6b5602
AlexLuthe <1005745@student.sae.edu.au>
2017-04-19 18:53:14 +1000

    Did some UI stuff. Options are a thing now

---
+++ Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuHandler : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Master Scene");
+        SceneManager.LoadScene("Cereal Mission", LoadSceneMode.Additive);
+        SceneManager.LoadScene("Coffee Mission", LoadSceneMode.Additive);
+        SceneManager.LoadScene("Toilet Mission", LoadSceneMode.Additive);
+        SceneManager.LoadScene("Shower Mission", LoadSceneMode.Additive);
+        SceneManager.LoadScene("Wake Up", LoadSceneMode.Additive);
+        SceneManager.LoadScene("End Game", LoadSceneMode.Additive);
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene("Options");
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void SaveOptions()
+    {
+        bool invertY = GameObject.Find("togInvertY").GetComponent<Toggle>().isOn;
+
+
+        using (System.IO.StreamWriter file = new System.IO.StreamWriter("Assets/options.txt", false))
+        {
+            if (invertY)
+                file.WriteLine("1");
+            else
+                file.WriteLine("0");
+        }
+    }
+}
