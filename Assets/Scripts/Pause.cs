28e388584e34510b367f25df55b2ae6d9a20a330
Alex Luthe <1005745@student.sae.edu.au>
2017-04-20 15:33:16 +1000

    Pause menu is a thing now

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour {
+
+    bool paused = false;
+    public GameObject pauseMenu;
+    public GameObject fpsController;
+    public GameObject camera;
+    public GameObject wakeUp;
+
+	// Use this for initialization
+	void Start () {
+        fpsController.GetComponent<Animation>().Play();
+        Animation[] eyelids = wakeUp.GetComponentsInChildren<Animation>();
+        for(int index = 0; index < eyelids.Length; ++index)
+            eyelids[index].Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.P)) {
+            if (paused) {
+                Time.timeScale = 1;
+                pauseMenu.SetActive(false);
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+                fpsController.SetActive(true);
+                camera.transform.parent = fpsController.transform;
+                camera.transform.position = fpsController.transform.position;
+                paused = false;
+            }
+            else {
+                Time.timeScale = 0;
+                pauseMenu.SetActive(true);
+                fpsController.SetActive(false);
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+                fpsController.SetActive(false);
+                camera.transform.parent = null;
+                paused = true;
+            }
+        }
+	}
+
+    public void Unpause() {
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        fpsController.SetActive(true);
+        camera.transform.parent = fpsController.transform;
+        camera.transform.position = fpsController.transform.position;
+        paused = false;
+    }
+}
